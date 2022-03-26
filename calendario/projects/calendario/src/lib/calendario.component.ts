05d55e7cc328996dbec3ca3b4d6539b5bb25623b
unknown <cristian.alb@zynksoftware.com>
2022-03-26 13:29:43 +0200

    Created caledario lib

---
+++ calendario/projects/calendario/src/lib/calendario.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cda-calendario',
+  template: `
+    <p>
+      calendario works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class CalendarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
