3f42799559a1c8195555865d71059203d520dbc1
unknown <cristian.alb@zynksoftware.com>
2022-06-01 12:14:08 +0300

    working on display event maximized functionality

--- calendario/projects/calendario/src/lib/views/my-calendar-event/my-calendar-event.component.ts
+++ calendario/projects/calendario/src/lib/views/my-calendar-event/my-calendar-event.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {EventItem} from "../../shared/models/event.model";
 
 @Component({
   selector: 'cda-my-calendar-event',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-calendar-event.component.css']
 })
 export class MyCalendarEventComponent implements OnInit {
+  @Input() event: EventItem | undefined;
+  @Input() getEventFields: any[] | undefined;
+  eventFields: any[] = [];
 
-  constructor() { }
+  constructor(public modalService: NgbModal) { }
 
   ngOnInit(): void {
+    // @ts-ignore
+    this.eventFields = this.getEventFields().filter((field: {key: string})=> field.key !== 'title');
   }
 
+  openModal(modal: any) {
+    this.modalService.open(modal);
+  }
+
+  getFieldValue(field: any | undefined): string {
+    if(field === undefined || this.event === undefined || this.event.originalEvent === undefined || this.event.originalEvent[field] === undefined)
+      return '---'
+    field = String(field);
+    return String(this.event.originalEvent[field])
+  }
+
+  getTimeOfDate(date: Date | undefined): string {
+    if(date === undefined)
+      return '---'
+    return `${date.getHours()}:${date.getMinutes()}`
+  }
 }
